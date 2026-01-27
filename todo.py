e08d4b507c7ddc227dd17b2b3fd8f23867e8c50c
Shin hyung Lee <shinhyunglee@Shins-MacBook-Air.local>
2026-01-27 13:12:35 -0800

    todo 1st part completed

--- todo.py
+++ todo.py
@@ -49,6 +49,38 @@ class TodoList:
     
     def __len__(self):
         return len(self._todos)
+    
+    def first(self):
+        return self._todos[0]
+
+    def last(self):
+        return self._todos[-1]
+    
+    def to_list(self):
+        return self._todos.copy()
+    
+    def todo_at(self, idx):
+        return self._todos[idx]
+    
+    def mark_done_at(self, idx):
+        self._todos[idx].done = True  
+    
+    def mark_undone_at(self, idx):
+        self._todos[idx].done = False
+        
+    def mark_all_done(self):
+        for todo in self._todos:
+            todo.done = True  
+    
+    def mark_all_undone(self):
+        for todo in self._todos:
+            todo.done = False  
+            
+    def all_done(self):
+        return all(todo.done for todo in self._todos) 
+    
+    def remove_at(self, idx):
+        self._todos.pop(idx)
 
 # Code omitted for brevity.
 
@@ -67,6 +99,160 @@ def setup():
     todo_list.add(todo3)
 
     return todo_list
+######## Setup Ends 
+
+def step_10():
+    print('--------------------------------- Step 10')
+    todo_list = setup()
+
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [X] Clean room
+    # [ ] Go to gym
+
+    todo_list.remove_at(1)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Go to gym
+
+    todo_list.remove_at(1)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+
+    try:
+        todo_list.remove_at(1)
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+    todo_list.remove_at(0)
+    print(todo_list)
+    # ---- Today's Todos -----
+
+step_10()
+
+# def step_9():
+#     print('--------------------------------- Step 9')
+#     todo_list = setup()
+
+#     print(todo_list.all_done())         # False
+
+#     todo_list.mark_all_done()
+#     print(todo_list.all_done())         # True
+
+#     todo_list.mark_undone_at(1)
+#     print(todo_list.all_done())         # False
+
+#     print(empty_todo_list.all_done())   # True
+
+# step_9()
+
+
+# def step_8():
+#     print('--------------------------------- Step 8')
+#     todo_list = setup()
+
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [ ] Buy milk
+#     # [X] Clean room
+#     # [ ] Go to gym
+
+#     todo_list.mark_all_done()
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [X] Buy milk
+#     # [X] Clean room
+#     # [X] Go to gym
+
+#     todo_list.mark_all_undone()
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [ ] Buy milk
+#     # [ ] Clean room
+#     # [ ] Go to gym
+
+# step_8()
+
+# def step_7():
+#     print('--------------------------------- Step 7')
+#     todo_list = setup()
+
+#     todo_list.mark_done_at(0)
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [X] Buy milk
+#     # [X] Clean room
+#     # [ ] Go to gym
+
+#     todo_list.mark_done_at(1)
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [X] Buy milk
+#     # [X] Clean room
+#     # [ ] Go to gym
+
+#     todo_list.mark_done_at(2)
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [X] Buy milk
+#     # [X] Clean room
+#     # [X] Go to gym
+
+#     try:
+#         todo_list.mark_done_at(3)
+#     except IndexError:
+#         print('Expected IndexError: Got it!')
+
+#     todo_list.mark_undone_at(0)
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [ ] Buy milk
+#     # [X] Clean room
+#     # [X] Go to gym
+
+#     todo_list.mark_undone_at(1)
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [ ] Buy milk
+#     # [ ] Clean room
+#     # [X] Go to gym
+
+#     todo_list.mark_undone_at(2)
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [ ] Buy milk
+#     # [ ] Clean room
+#     # [ ] Go to gym
+
+#     try:
+#         todo_list.mark_undone_at(3)
+#     except IndexError:
+#         print('Expected IndexError: Got it!')
+
+# step_7()
+
+
+
+# def step_6():
+#     print('--------------------------------- Step 6')
+#     todo_list = setup()
+
+#     print(todo_list.todo_at(0))        # [ ] Buy milk
+#     print(todo_list.todo_at(1))        # [X] Clean room
+#     print(todo_list.todo_at(2))        # [ ] Go to gym
+
+#     try:
+#         todo_list.todo_at(3)
+#     except IndexError:
+#         print('Expected IndexError: Got it!')
+
+#     # Ensure we have a reference
+#     print(todo_list.todo_at(1) is todo_list.todo_at(1))  # True
+
+# step_6()
 
 # def step_1():
 #     print('--------------------------------- Step 1')
@@ -96,12 +282,47 @@ def setup():
 
 # step_2()
     
-def step_3():
-    print('--------------------------------- Step 3')
-    todo_list = setup()
+# def step_3():
+#     print('--------------------------------- Step 3')
+#     todo_list = setup()
 
-    print(len(todo_list))              # 3
-    print(len(empty_todo_list))        # 0
+#     print(len(todo_list))              # 3
+#     print(len(empty_todo_list))        # 0
 
-step_3()
+# step_3()
+
+# def step_4():
+#     print('--------------------------------- Step 4')
+#     todo_list = setup()
+
+#     print(todo_list.first())           # [ ] Buy milk
+#     print(todo_list.last())            # [ ] Go to gym
+
+#     try:
+#         empty_todo_list.first()
+#     except IndexError:
+#         print('Expected IndexError: Got it!')
+
+#     try:
+#         empty_todo_list.last()
+#     except IndexError:
+#         print('Expected IndexError: Got it!')
+
+# step_4()
+
+# def step_5():
+#     print('--------------------------------- Step 5')
+#     todo_list = setup()
+
+#     print(empty_todo_list.to_list())    # []
+
+#     todos = todo_list.to_list()
+#     print(type(todos).__name__)         # list
+
+#     for todo in todos:
+#         print(todo)                     # [ ] Buy milk
+#                                         # [X] Clean room
+#                                         # [ ] Go to gym
+
+# step_5()
     
